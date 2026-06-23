39b4b3e7e50c9cfec87a0ba6360aee77231c7327
sakshikawdker86 <kawdkersakshi86@gmail.com>
2026-06-23 17:45:08 +0530

    Add PostgreSQL connection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const pool = require('./db');
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    status: "running"
-  });
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 const PORT = 3000;
